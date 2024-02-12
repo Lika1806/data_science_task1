fc1c12f63ab6706dd61d23f51d1864d32811a1f5
Angela (Lika) <97836245+Lika1806@users.noreply.github.com>
2024-02-12 14:05:55 +0400

    Add files via upload

---
+++ data_science_task1.py
@@ -0,0 +1,54 @@
+passing_grade = 75
+additional_tutoring_grade = 85
+student_grades = {
+    "Alice": 88,
+    "Bob": 76,
+    "Charlie": 90,
+    "Diana": 85,
+    "Evan": 92,
+    "Fiona": 79,
+    "George": 95
+}
+
+grades = list(student_grades.values())
+max_grade = max(grades)
+min_grade = min(grades)
+average_grade = sum(grades) / len(student_grades)
+failed = []
+passed=[]
+first_students = []
+last_students = []
+additional_tutoring = []
+
+for student, grade in student_grades.items():
+    #finding student(s) with the highest grade
+    if grade == max_grade:
+        first_students.append(student)
+    #finding student(s) with the lowest grade
+    elif grade == min_grade:
+        last_students.append(student)
+    #finding passed students
+    if grade>=passing_grade:
+        passed.append(student)
+    #finding failed students
+    else:
+        failed.append(student)
+    #finding students that need suggestions
+    if grade < additional_tutoring_grade:
+        additional_tutoring.append(student)
+
+#part1
+print(f"GRADE SUMMARY")
+print(f"the average grade for the class:        {average_grade:.2f}")
+print(f"student(s) with the highest grade:      {first_students}")
+print(f"student(s) with the lowest grade:       {last_students}")
+print(f"GRADE DISTRIBUTION")
+print(f"Students that passed:   {passed}")
+print(f"Students that failed:   {failed}")
+print(f"IMPROVEMENT SUGGESTIONS")
+for student in additional_tutoring:
+    print(f'- Dear {student}, you\'ve got {student_grades[student]} points in exam, consider attending additional tutoring sessions.')
+print(f"HERE ARE ALL STUDENTS AND ALL GRADES")
+max_length = max(len(name) for name in student_grades.keys())
+for student, grade in student_grades.items():
+    print(f"{student.ljust(max_length)}: {'=' * int(grade//5)}")
